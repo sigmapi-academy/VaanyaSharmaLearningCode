d5e12eaef1edebcc92747c932f739de947c7cc78
sigmapi-academy <kumarsatyendermishra@gmail.com>
2026-07-15 07:43:30 +0530

    Discussed double recursion

---
+++ Coding/Recursion/FibonacciSeries.py
@@ -0,0 +1,22 @@
+
+def fibonacci(n:int):
+    # base cases
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+def numberOfterms(start:int, n:int):
+    if start <= n:
+        print(fibonacci(start))
+        numberOfterms(start+1, n)
+        
+
+# main code
+n = int(input('Enter number of terms'))
+
+numberOfterms(1, n)
+
+ 
